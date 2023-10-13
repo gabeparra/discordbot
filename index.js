be1e8b4d3e2b98e9d715c7636d9a767bb699ec9d
Gabriel Parra <ma.parrar16@gmail.com>
2023-10-13 15:40:33 -0400

    more potential fixes

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const path = require('path');
 const fs = require('fs');
 const { Client: DiscordClient, Collection, Events, GatewayIntentBits } = require("discord.js");
 const { token } = require("./config.json");
-const { Pool, Client:PgClient } =require('pg');
+const { Pool, Client: PgClient } = require('pg');
 
 
 // Create a new client instance
 const discordclient = new DiscordClient({
-  intents: [GatewayIntentBits.Guilds,GatewayIntentBits.MessageContent,GatewayIntentBits.GuildMessages,],
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages,],
 });
 
 discordclient.commands = new Collection();
@@ -19,26 +19,26 @@ discordclient.commands = new Collection();
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 for (const folder of commandFolders) {
-	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-	for (const file of commandFiles) {
-		const filePath = path.join(commandsPath, file);
-		const command = require(filePath);
-		if ('data' in command && 'execute' in command) {
-			discordclient.commands.set(command.data.name, command);
-		} else {
-			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-		}
-	}
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if ('data' in command && 'execute' in command) {
+      discordclient.commands.set(command.data.name, command);
+    } else {
+      console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    }
+  }
 }
 
 // When the client is ready, run this code (only once)
 discordclient.once("ready", () => {
   console.log(`Ready! Logged in as ${discordclient.user.tag}`);
-  
+
   // Log the bot's clientId
   console.log(`Client ID: ${discordclient.user.id}`);
-  
+
   // Log the IDs of all the guilds the bot is in
   discordclient.guilds.cache.forEach(guild => {
     console.log(`Guild ID: ${guild.id}`);
@@ -53,25 +53,19 @@ discordclient.on(Events.InteractionCreate, async interaction => {
   if (!interaction.isChatInputCommand()) return;
   // Get the guild from the interaction
   const { guild } = interaction;
-  
+
   // Log the guild ID
   console.log(`Interaction received from guild: ${guild.id}`);
 
   const command = discordclient.commands.get(interaction.commandName);
-  
 
-	if (!command) return;
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
-	}
+  if (!command) return;
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+  }
 });
 
 
@@ -97,7 +91,7 @@ discordclient.on("messageCreate", async (message) => {
   }
   if (message.content.toLowerCase().includes("prayer")) {
     message.channel.send({
-      files: [{attachment: "https://media.discordapp.net/attachments/539934160534372412/973009788810182696/IMG_8403.gif?width=667&height=805",name:'SPOILER_prayer.gif'}],
+      files: [{ attachment: "https://media.discordapp.net/attachments/539934160534372412/973009788810182696/IMG_8403.gif?width=667&height=805", name: 'SPOILER_prayer.gif' }],
     });
   }
   if (message.content.toLowerCase().includes("we live we love we lie")) {
@@ -121,16 +115,6 @@ discordclient.on("messageCreate", async (message) => {
     message.channel.send({
       files: ["https://im.ezgif.com/tmp/ezgif-1-77191792b3.gif"],
     });
-  }
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    if (interaction.replied || interaction.deferred) {
-      await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-    } else {
-      await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-    }
   }
 });
 
